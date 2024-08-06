fd0d96129bb4e45507d65143e123a79fb3b9f506
José Malta <00001090762586sp@al.educacao.sp.gov.br>
2024-08-06 20:24:39 -0300

    aprimorando_main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from pessoa import Pessoa
+
 print("0lá somos a equipe dos power ranger nós somos buxos e vamos descobrir sua data de nacimento.")
 
-calculator = int(input("Digite sua idade:"))
 
-calculator2 = input("Voce fez aniversario esse ano?:")
+calculator2 = input("Voce fez aniversario esse ano?\nDigite sua resposta:").capitalize
 
 
 if calculator2 == "Sim":
-    print(f"Voce nasceu em {2024 - calculator}")
+    Pessoa.ano1()
 elif calculator2 == "Não":
-    print("")
+    Pessoa.ano2()
