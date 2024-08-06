9db798d67da4bedfadbace1afbafab1c6808ad76
Samuel Augusto <eusamuelaugusto@gmail.com>
2024-08-06 20:19:16 -0300

    add_class_pessoa

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-
+print("0lá somos a equipe dos power ranger nós somos buxos e vamos descobrir sua data de nacimento.")
 
 calculator = int(input("Digite sua idade:"))
 
 calculator2 = input("Voce fez aniversario esse ano?:")
 
 
-
 if calculator2 == "Sim":
-    print(f"Voce nasceu em {2024 - calculator} ")
+    print(f"Voce nasceu em {2024 - calculator}")
 elif calculator2 == "Não":
-    
-
+    print("")
---
+++ pessoa.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    def _init_(self) -> None:
+        pass
+
+
+    def ano1():
+     calculator = int(input("Digite sua idade:\nDigite sua resposta:"))
+     print(f"Voce nasceu em {2024 - calculator} ")
+
+    def ano2():
+     calculator = int(input("Digite sua idade:\nDigite sua resposta:"))
+     print(f"Voce nasceu em {2024 - calculator} ")
